32e0b0b17da02e10c725497c98a2aad76fd4fe72
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-06-27 13:57:19 +0300

    Normalize Flat.phonenumbers into Flat.owner_pure_phone

--- property/models.py
+++ property/models.py
@@ -49,10 +49,10 @@ class Flat(models.Model):
         blank=True,
         db_index=True)
     new_building = models.BooleanField('Новая-ли постройка', blank=True, null=True)
-    liked_by = models.ManyToManyField(User, related_name='Liked_flats', verbose_name='Кто лайкнул')
+    liked_by = models.ManyToManyField(User, related_name='Liked_flats', verbose_name='Кто лайкнул', null=True, blank=True)
     
     def __str__(self):
-        return f'{self.town}, {self.address} ({self.price}р.)'
+        return f'{self.town}, {self.address} ({self.price}р.) {self.owner_pure_phone} {self.owners_phonenumber}'
 
 
 class Complaint(models.Model):
