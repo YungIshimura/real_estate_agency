6afe5f19c8479888682c2f5b0f9399e738e49fa8
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-06-28 14:25:26 +0300

    Migrate db_index to fields

--- property/migrations/0009_auto_20220627_1425.py
+++ property/migrations/0009_auto_20220627_1425.py
@@ -21,9 +21,9 @@ class Migration(migrations.Migration):
             name='Owner',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('owner', models.CharField(max_length=200, verbose_name='ФИО владельца')),
-                ('owner_pure_phone', phonenumber_field.modelfields.PhoneNumberField(blank=True, max_length=128, region=None, verbose_name='Нормализированный номер владельца')),
-                ('owners_phonenumber', models.CharField(blank=True, max_length=20, verbose_name='Номер владельца')),
+                ('owner', models.CharField(max_length=200, db_index=True, verbose_name='ФИО владельца')),
+                ('owner_pure_phone', phonenumber_field.modelfields.PhoneNumberField(blank=True, max_length=128, region=None, db_index=True, verbose_name='Нормализированный номер владельца')),
+                ('owners_phonenumber', models.CharField(blank=True, max_length=20, db_index=True, verbose_name='Номер владельца')),
                 ('flats', models.ManyToManyField(related_name='flats', to='property.Flat', verbose_name='Квартиры в собственности')),
             ],
         ),
