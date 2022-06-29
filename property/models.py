990b1b8ce87fee1c4d6ec19aa10b90359e08bb6d
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-06-29 20:43:06 +0300

    Add related_name

--- property/models.py
+++ property/models.py
@@ -59,9 +59,11 @@ class Flat(models.Model):
 
 class Complaint(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,
-                             verbose_name='Кто жаловался')
+                             verbose_name='Кто жаловался',
+                             related_name='complaining_user')
     flat = models.ForeignKey(Flat, on_delete=models.CASCADE,
-                             verbose_name='Квартира, на которую жаловаилсь')
+                             verbose_name='Квартира, на которую жаловаилсь',
+                             related_name='complained_flat')
     compaint_text = models.TextField(max_length=250, blank=True,
                                      verbose_name='Текст жалобы')
 
